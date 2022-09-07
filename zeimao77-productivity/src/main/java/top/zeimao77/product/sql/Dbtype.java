367867b88b68577c3d70a290c8ddd0771a0ce932
root <root@zeimao77-pc.localdomain>
2022-09-07 14:25:35 +0800

    dbtype

--- zeimao77-productivity/src/main/java/top/zeimao77/product/sql/Dbtype.java
+++ zeimao77-productivity/src/main/java/top/zeimao77/product/sql/Dbtype.java
@@ -1,16 +1,16 @@
 package top.zeimao77.product.sql;
 
-public class Dbtype {
+public interface Dbtype {
 
-     public static final Integer ALL = 0x7FFFFFFF;   /* 所有数据库 */
+     Integer ALL = 0x7FFFFFFF;   /* 所有数据库 */
 
-     public static final Integer MYSQL = 0x00000001;  /* ORACLE MYSQL */
-     public static final Integer ORACLE = 0x00000002;  /* Oracle */
-     public static final Integer SQLITE = 0x00000004;  /* SQLite */
-     public static final Integer POSTGRESQL = 0x00000008;  /* PostgreSQL */
-     public static final Integer SQLSERVER = 0x00000010;  /* Microsoft SQL server */
-     public static final Integer ACCESS = 0x00000020;  /* Microsoft Access */
-     public static final Integer MARIADB = 0x0000040;  /* MariaDB */
+     Integer MYSQL = 0x00000001;  /* ORACLE MYSQL */
+     Integer ORACLE = 0x00000002;  /* Oracle */
+     Integer SQLITE = 0x00000004;  /* SQLite */
+     Integer POSTGRESQL = 0x00000008;  /* PostgreSQL */
+     Integer SQLSERVER = 0x00000010;  /* Microsoft SQL server */
+     Integer ACCESS = 0x00000020;  /* Microsoft Access */
+     Integer MARIADB = 0x0000040;  /* MariaDB */
 
 
 }
