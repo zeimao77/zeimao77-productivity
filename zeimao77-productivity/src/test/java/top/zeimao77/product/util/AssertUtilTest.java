de0d02e2e7da4f72a5b430d01749b1ddf9428a1d
zeimao77 <zeimao77@foxmail.com>
2023-01-05 17:36:36 +0800

    1

--- zeimao77-productivity/src/test/java/top/zeimao77/product/util/AssertUtilTest.java
+++ zeimao77-productivity/src/test/java/top/zeimao77/product/util/AssertUtilTest.java
@@ -6,6 +6,7 @@ import top.zeimao77.product.fileio.serialize.SerializeWriter;
 import top.zeimao77.product.fileio.serialize.Type;
 
 import top.zeimao77.product.main.BaseMain;
+import top.zeimao77.product.mysql.DemoModel;
 
 import java.io.*;
 import java.lang.reflect.Field;
@@ -20,22 +21,6 @@ public class AssertUtilTest extends BaseMain {
 
     public static void main(String[] args) throws IOException, NoSuchFieldException, IllegalAccessException {
         SerializeWriter writer = new SerializeWriter();
-        String j = "{\"int_val\":123874,\"dec_val\":\"-1782.3450000000001223450\",\"dou_val\":11.1434,\"fki_val\":3.1415926,\"arr_val\":[1,\"hello\",33.14,true,'c',1.768,23,\"2022-01-04\"],\"hw_val\":\"helloworld\"}";
-        logger.info("j:{}",j.length());
-        HashMap<String, Object> map = new HashMap<>();
-        map.put("int_val",123874);
-        map.put("dou_val",11.14D);
-        map.put("flo_val",3.1415926F);
-
-        map.put("arr_val", Arrays.asList(1,"hello",33.14,true,'c',1.768F,23L, LocalDateTime.now()));
-        map.put("hw_val","hellworld");
-        map.put("dec_val","-1782.3450000000001223450");
-
-        writer.writeInt32(123);
-        writer.writeDouble(11.1434D);
-        writer.writeFloat(3.1415926F);
-        writer.writeList(Arrays.asList(1,"hello",33.14,true,'c',1.768F,23L, LocalDateTime.now()));
-        writer.writeString("helloworld");
         BigDecimal decimal = new BigDecimal("-1782.3450000000001223450");
         writer.writeNext(SerializeUtil.BIGDECIMAL_DATA_SERIALIZE,decimal, Type.DECIMAL.getTypeValue());
 
@@ -63,14 +48,15 @@ public class AssertUtilTest extends BaseMain {
         BigInteger iv = (BigInteger) unsafe.getObject(decimal, l);
         logger.info("{}",iv.toString());
 
-        /**
-        intVal.setAccessible(true);
-        BigInteger bigInteger1 = (BigInteger) intVal.get(decimal);
-         logger.info("{}",bigInteger1.toString());
-         **/
+        Field demoId = DemoModel.class.getDeclaredField("demoId");
+        demoId.setAccessible(true);
+
+
+         
 
         logger.info("字符串序列化长度:{}byte",decimal.toString().length() * 2);
         logger.info("decimal序列化长度:{}byte",1+2+iv.toByteArray().length);
+        logger.info("{}",BeanUtil.getProperty(decimal,"intVal"));
 
         int scale = decimal.scale();
         BigDecimal bigDecimal = new BigDecimal(iv, scale);
