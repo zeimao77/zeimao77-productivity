c71cff238e6a8eb7ef6e414c77413da5c5089dbc
zeimao77 <zeimao77@foxmail.com>
2023-01-09 15:40:43 +0800

    5ok

--- app-main/src/main/java/com/zeimao77/Main.java
+++ app-main/src/main/java/com/zeimao77/Main.java
@@ -7,19 +7,13 @@ public class Main extends BaseMain {
 
     public static void main(String[] args) {
         BaseMain.showBanner();
-
-        new Thread(() -> {
-            while (true) {
-                delay_ms(2000);
-                logger.info(LongIdGenerator.INSTANCE.generate());
-            }
-        }).start();
-        while(true) {
-            delay_ms(2000);
-            logger.info(Long.toHexString(System.currentTimeMillis()));
-        }
+        logger.trace(LongIdGenerator.INSTANCE.generate());
+        logger.debug(LongIdGenerator.INSTANCE.generate());
+        logger.info(LongIdGenerator.INSTANCE.generate());
+        logger.warn(LongIdGenerator.INSTANCE.generate());
+        logger.error(LongIdGenerator.INSTANCE.generate());
+        logger.fatal(LongIdGenerator.INSTANCE.generate());
     }
-
 
 
 }
