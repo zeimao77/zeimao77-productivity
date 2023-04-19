8658a8a0a12b6f47b8c3a619da5459aa8074fd1b
zeimao77 <zeimao77@foxmail.com>
2023-04-19 11:04:37 +0800

    deleteautobean

--- zeimao77-productivity/src/main/java/top/zeimao77/product/factory/BeanFactory.java
+++ zeimao77-productivity/src/main/java/top/zeimao77/product/factory/BeanFactory.java
@@ -96,26 +96,6 @@ public class BeanFactory {
         Supplier<?> supplier = prototypesFactory.get(beanName);
         if(supplier != null)
             return (T)supplier.get();
-        if(requiredType == SimpleSqlClient.class && beanName.startsWith(ComponentFactory.AUTOBEAN_SQLCLIENT)) {
-            SimpleSqlClient client = ComponentFactory.initSimpleSqlClient(beanName, null);
-            registerSingleton(beanName,client);
-            return (T) client;
-        }
-        if(requiredType == SimpleSqlTemplate.class && beanName.startsWith(ComponentFactory.AUTOBEAN_SQLTEMPLATE)) {
-            SimpleSqlTemplate client = ComponentFactory.initSimpleSqlTemplate(beanName, null);
-            registerSingleton(beanName,client);
-            return (T) client;
-        }
-        if("redis.clients.jedis.Jedis".equals(requiredType.getName())) {
-            return (T) ComponentFactory.initJedis(beanName,this);
-        }
-        if("redis.clients.jedis.JedisCluster".equals(requiredType.getName())) {
-            return (T) ComponentFactory.initJedisCluster(beanName,this);
-        }
-        if(requiredType == SimpleEmailSender.class) {
-            SimpleEmailSender simpleEmailSender = ComponentFactory.initSimpleEmailSender(beanName, this);
-            return (T) simpleEmailSender;
-        }
         throw new BaseServiceRunException(WRONG_ACTION,"没有这样的BEAN实例:"+beanName);
     }
 
