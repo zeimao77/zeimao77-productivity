9b119dcab46b457ebf7c739785ced16138a7aa56
zeimao77 <zeimao77@foxmail.com>
2023-09-06 14:38:18 +0800

    1

--- zeimao77-productivity/src/main/java/top/zeimao77/product/fileio/oexcel/ExcelXlsxDocumentBuilder.java
+++ zeimao77-productivity/src/main/java/top/zeimao77/product/fileio/oexcel/ExcelXlsxDocumentBuilder.java
@@ -172,8 +172,11 @@ public class ExcelXlsxDocumentBuilder implements XlsxDocumentBuilder{
     }
 
     private void setHead() {
-        Row row = newRow();
         List<Table.Column> columnList = table.getColumnList();
+        if(columnList == null || columnList.isEmpty()) {
+            return;
+        }
+        Row row = newRow();
         for (Table.Column column : columnList) {
             Cell cell = row.createCell(column.getIndex());
             cell.setCellValue(column.getTitle());
@@ -223,6 +226,7 @@ public class ExcelXlsxDocumentBuilder implements XlsxDocumentBuilder{
     public Map<Table.Column, IConverter> getConverterMap() {
         return converterMap;
     }
+
 
 
 }
