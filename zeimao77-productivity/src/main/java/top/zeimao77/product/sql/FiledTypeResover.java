6a926c8d93ebc10e2707d2b0060c239f17ddc782
zeimao77 <zeimao77@foxmail.com>
2024-10-21 17:23:43 +0800

    1

--- zeimao77-productivity/src/main/java/top/zeimao77/product/sql/FiledTypeResover.java
+++ zeimao77-productivity/src/main/java/top/zeimao77/product/sql/FiledTypeResover.java
@@ -1,13 +1,27 @@
 package top.zeimao77.product.sql;
 
+import org.slf4j.Logger;
+import top.zeimao77.product.json.Ijson;
 import top.zeimao77.product.model.Orderd;
+import top.zeimao77.product.util.CalendarDateUtil;
+import top.zeimao77.product.util.LocalDateTimeUtil;
+import top.zeimao77.product.util.StringOptional;
 
 import java.lang.reflect.ParameterizedType;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.time.*;
 
 /**
  * 通过目标类型的字段值解析器接口
  */
 public interface FiledTypeResover<T> extends Orderd {
+
+    Logger logger = org.slf4j.LoggerFactory.getLogger(FiledTypeResover.class);
 
     default Class<T> getTclass() {
         Class<T> tClass = (Class<T>)((ParameterizedType)getClass().getGenericInterfaces()[0]).getActualTypeArguments()[0];
@@ -32,4 +46,319 @@ public interface FiledTypeResover<T> extends Orderd {
      * @return 解析结果
      */
     T resove(Object obj);
+
+    FiledTypeResover STRINGTYPERESOVER = new FiledTypeResover<String>() {
+        @Override
+        public String resove(Object obj) {
+            return obj == null ? null : obj.toString();
+        }
+
+        @Override
+        public int orderd() {
+            return 1000;
+        }
+    };
+    FiledTypeResover LONGTYPERESOVER = new FiledTypeResover<Long>() {
+        @Override
+        public Long resove(Object obj) {
+            if(obj == null)
+                return null;
+            if(obj instanceof Integer o) {
+                return o.longValue();
+            }
+            if(obj instanceof Long o) {
+                return o;
+            }
+            if(obj instanceof Timestamp o) {
+                return o.getTime();
+            }
+            if(obj instanceof LocalDateTime o) {
+                return o.toEpochSecond(ZoneId.systemDefault().getRules().getOffset(Instant.now()));
+            }
+            return Long.valueOf(obj.toString());
+        }
+
+        @Override
+        public int orderd() {
+            return 1100;
+        }
+    };
+    FiledTypeResover INTEGTYPERESOVER = new FiledTypeResover<Integer>() {
+        @Override
+        public Integer resove(Object obj) {
+            if(obj == null)
+                return null;
+            if(obj instanceof Integer o) {
+                return o;
+            }
+            if(obj instanceof Long o) {
+                return o.intValue();
+            }
+            return Integer.valueOf(obj.toString());
+        }
+
+        @Override
+        public int orderd() {
+            return 1200;
+        }
+    };
+    FiledTypeResover DOUBLETYPERESOVER = new FiledTypeResover<Double>() {
+
+        @Override
+        public Double resove(Object obj) {
+            if(obj == null)
+                return null;
+            if(obj instanceof Number o)
+                return o.doubleValue();
+            return Double.valueOf(obj.toString());
+        }
+
+        @Override
+        public int orderd() {
+            return 1300;
+        }
+    };
+    FiledTypeResover BIGDECIMALTYPERESOVER = new FiledTypeResover<BigDecimal>() {
+        @Override
+        public BigDecimal resove(Object obj) {
+            if( obj == null )
+                return null;
+            if(obj instanceof BigDecimal o)
+                return o;
+            if(obj instanceof Number o)
+                return BigDecimal.valueOf(o.doubleValue());
+            return new BigDecimal(obj.toString());
+        }
+
+        @Override
+        public int orderd() {
+            return 1400;
+        }
+    };
+    FiledTypeResover BOOLEANTYPERESOVER = new FiledTypeResover<Boolean>() {
+
+        @Override
+        public Boolean resove(Object obj) {
+            if( obj == null )
+                return null;
+            if(obj instanceof Boolean o)
+                return o;
+            if(obj instanceof Number o)
+                return o.intValue() == 0 ? false : true;
+            if(obj instanceof String o)
+                return Boolean.valueOf(o);
+            return null;
+        }
+
+        @Override
+        public int orderd() {
+            return 1500;
+        }
+    };
+    FiledTypeResover DATETYPERESOVER = new FiledTypeResover<java.util.Date>() {
+        @Override
+        public java.util.Date resove(Object obj) {
+            if(obj == null)
+                return null;
+            if(obj instanceof LocalDateTime o)
+                return CalendarDateUtil.fromLocalDateTime(o);
+            if(obj instanceof Time o)
+                return new Date(o.getTime());
+            if(obj instanceof Date o)
+                return new Date(o.getTime());
+            if(obj instanceof Timestamp o)
+                return new Date(o.getTime());
+            if(obj instanceof Long o) {
+                return Date.from(Instant.ofEpochMilli(o));
+            }
+            if(obj instanceof String o)
+                return CalendarDateUtil.parseDateTime(o);
+            logger.error("无法解析的日期时间：{}",obj);
+            return null;
+        }
+
+        @Override
+        public int orderd() {
+            return 1600;
+        }
+    };
+    FiledTypeResover LOCALDATETIMETYPERESOVER = new FiledTypeResover<LocalDateTime>() {
+        @Override
+        public LocalDateTime resove(Object obj) {
+            if(obj == null) return null;
+            if(obj instanceof LocalDateTime o)
+                return o;
+            if(obj instanceof LocalDate o) {
+                return LocalDateTime.of(o, LocalTime.MIN);
+            }
+            if(obj instanceof Timestamp o)
+                return LocalDateTime.ofInstant(Instant.ofEpochMilli(o.getTime()), ZoneId.systemDefault());
+            if(obj instanceof Long o) {
+                return LocalDateTime.ofInstant(Instant.ofEpochMilli(o),ZoneId.systemDefault());
+            }
+            if(obj instanceof String o)
+                return LocalDateTime.parse(o, LocalDateTimeUtil.STANDARDDATETIMEFORMATTER);
+            logger.error("无法解析的日期时间：{}",obj);
+            return null;
+        }
+        @Override
+        public int orderd() {
+            return 1700;
+        }
+    };
+
+    FiledTypeResover LOCALTIMETYPERESOVER = new FiledTypeResover<LocalTime>() {
+        @Override
+        public LocalTime resove(Object obj) {
+            if(obj == null) return null;
+            if(obj instanceof LocalTime o)
+                return o;
+            if(obj instanceof Time o)
+                return o.toLocalTime();
+            if(obj instanceof String o)
+                return LocalTime.parse(o,LocalDateTimeUtil.STANDARDTIMEFORMATTER);
+            logger.error("无法解析的时间：{}",obj);
+            return null;
+        }
+
+        @Override
+        public int orderd() {
+            return 1800;
+        }
+    };
+
+    FiledTypeResover LOCALDATETYPERESOVER = new FiledTypeResover<LocalDate>() {
+        @Override
+        public LocalDate resove(Object obj) {
+            if(obj == null) return  null;
+            if(obj instanceof LocalDate o)
+                return o;
+            if(obj instanceof Date o)
+                return o.toLocalDate();
+            if(obj instanceof String o)
+                return LocalDate.parse(o,LocalDateTimeUtil.STANDARDDATEFORMATTER);
+            logger.error("无法解析的日期：{}",obj);
+            return null;
+        }
+
+        @Override
+        public int orderd() {
+            return 1900;
+        }
+    };
+    FiledTypeResover FLOATTYPERESOVER = new FiledTypeResover<Float>() {
+        @Override
+        public Float resove(Object obj) {
+            if(obj == null)
+                return null;
+            if(obj instanceof Float o)
+                return o;
+            if(obj instanceof Number o)
+                return o.floatValue();
+            return Float.valueOf(obj.toString());
+        }
+
+        @Override
+        public int orderd() {
+            return 2000;
+        }
+    };
+    FiledTypeResover SHORTTYPERESOVER = new FiledTypeResover<Short>() {
+        @Override
+        public Short resove(Object obj) {
+            if(obj == null)
+                return null;
+            if(obj instanceof Short o)
+                return o;
+            if(obj instanceof Number o)
+                return o.shortValue();
+            return Short.valueOf(obj.toString());
+        }
+
+        @Override
+        public int orderd() {
+            return 2100;
+        }
+    };
+    FiledTypeResover CHARTYPERESOVER = new FiledTypeResover<Character>() {
+        @Override
+        public Character resove(Object obj) {
+            return obj == null ? null : Character.valueOf(obj.toString().charAt(0));
+        }
+
+        @Override
+        public int orderd() {
+            return 2200;
+        }
+    };
+    FiledTypeResover BIGINTTYPERESOVER = new FiledTypeResover<BigInteger>() {
+
+        @Override
+        public BigInteger resove(Object obj) {
+            if(obj instanceof BigInteger o)
+                return o;
+            if(obj instanceof Number o) {
+                return BigInteger.valueOf(o.longValue());
+            }
+            return obj == null ? null : new BigInteger(obj.toString());
+        }
+
+        @Override
+        public int orderd() {
+            return 2300;
+        }
+    };
+    FiledTypeResover BYTEBUFFERTYPERESOVER = new FiledTypeResover<ByteBuffer>() {
+
+        @Override
+        public ByteBuffer resove(Object obj) {
+            if(obj == null) return null;
+            if(obj.getClass().isArray() && obj.getClass().getComponentType() == byte.class) {
+                return ByteBuffer.wrap((byte[])obj);
+            }
+            return null;
+        }
+
+        @Override
+        public int orderd() {
+            return 2400;
+        }
+    };
+    FiledTypeResover FIELDTYPERESOVER = new FiledTypeResover<StringOptional>() {
+
+        @Override
+        public StringOptional resove(Object obj) {
+            if(obj == null) return StringOptional.empty();
+            return new StringOptional(obj.toString());
+        }
+
+        @Override
+        public int orderd() {
+            return 2500;
+        }
+    };
+
+    FiledTypeResover IJSONTYPERESOVER = new FiledTypeResover<Ijson>() {
+        @Override
+        public Ijson resove(Object obj) {
+            return Ijson.parse(obj.toString());
+        }
+
+        @Override
+        public int orderd() {
+            return 2600;
+        }
+    };
+
+    FiledTypeResover OBJECTTYPERESOVER = new FiledTypeResover<Object>() {
+        @Override
+        public Object resove(Object obj) {
+            return obj;
+        }
+
+        @Override
+        public int orderd() {
+            return 5000;
+        }
+    };
 }
--- zeimao77-productivity/src/main/java/top/zeimao77/product/sql/ResultSetResolve.java
+++ zeimao77-productivity/src/main/java/top/zeimao77/product/sql/ResultSetResolve.java
@@ -2,7 +2,6 @@ package top.zeimao77.product.sql;
 
 import java.sql.ResultSet;
 import java.util.List;
-import java.util.Map;
 
 public interface ResultSetResolve {
 
@@ -15,20 +14,5 @@ public interface ResultSetResolve {
      */
     <T> void populate(ResultSet rs , Class<T> clazz, List<T> list);
 
-    /**
-     * 将ResultSet对象转一个MAP对象
-     * @param rs 结果集
-     * @param list 转换后结果的存放容器
-     */
-    void populateMap(ResultSet rs,List<Map<String,Object>> list);
-
-    /**
-     * 将一个数据库对象转JAVA对象，在存储过程出参时使用
-     * @param value 数据库对象
-     * @param clazz 目标对象类型
-     * @return 结果
-     * @param <T> 对象类型
-     */
-    <T> T resolve(Object value,Class<T> clazz);
 
 }
